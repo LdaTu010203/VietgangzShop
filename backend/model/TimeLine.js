fc79aad752ae3584df0d69e63f624762753f59e5
Anh Tú <leduonganhtu0102@gmail.com>
2025-07-31 01:16:52 +0700

    Revert "fix and update"
    This reverts commit 02f25fb3cb11808cbb036ad56b7488c935d344be.

--- backend/model/TimeLine.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const timelineSchema = new mongoose.Schema({
-    status: { type: String, required: true },
-    time: { type: Date, required: true },
-    text: { type: String, required: true }
-});
-
-module.exports = timelineSchema;
--- frontend/src/utils/search/search.js
+++
@@ -1,6 +0,0 @@
-export const Search = (products, searchTerm) => {
-    if (!searchTerm) return products;
-    return products.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-};
